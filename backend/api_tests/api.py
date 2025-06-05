e711638470039da5b0425c7abd25733180239584
Omar Ouaissa <o.ouaissa@vu.nl>
2025-06-05 14:19:38 +0200

    Test api

--- backend/api_tests/api.py
+++ backend/api_tests/api.py
@@ -5,5 +5,5 @@ import sys
 import optparse
 
 book = requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAI8gKE3AQCSaGhCigcNXRq57OOWmHaT-g")
-print(book)
-
+print(book.json)
+book    
