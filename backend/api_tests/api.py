3eabbf650e2917e51afb6b515891db529bd5e3a4
Omar Ouaissa <o.ouaissa@vu.nl>
2025-06-05 15:15:47 +0200

    Book Api Test (Type shit)

--- backend/api_tests/api.py
+++ backend/api_tests/api.py
@@ -4,6 +4,8 @@ import requests
 import sys
 import optparse
 
-book = requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAI8gKE3AQCSaGhCigcNXRq57OOWmHaT-g")
-print(book.json)
-book    
+book = requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=Words&key=AIzaSyDYyS2cfYO-2z7dD8iEsmmzv5zYMPAbU1Y")
+response = book.json()
+
+print(response["items"][0])
+
