f8e8313e6f7a17aa85b9e1ce8b8108b581e89ca3
Omar Ouaissa <o.ouaissa@vu.nl>
2025-06-06 12:36:17 +0200

    Added the first test. And the settings to setup pytest on vscode

--- backend/tests/tests.py
+++ backend/tests/tests.py
@@ -1,1 +1,11 @@
 ### first backend tests file ###
+import requests
+import unittest
+
+class tests(unittest.TestCase):
+    def test_api_response(self):
+        # This tests that the api give a 200 (ok) status code
+        assert requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAI8gKE3AQCSaGhCigcNXRq57OOWmHaT-g").status_code == 200
+
+if __name__ == "__main__":
+    unittest.main
