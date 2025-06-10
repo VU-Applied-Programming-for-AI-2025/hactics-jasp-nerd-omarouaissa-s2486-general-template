d6894c9f2c3c2dc79b0b8067ef6295a0cab7559d
Omar Ouaissa <o.ouaissa@vu.nl>
2025-06-10 15:15:18 +0200

    book_search test

--- backend/tests/test_tests.py
+++ backend/tests/test_tests.py
@@ -15,6 +15,15 @@ def test_api_response():
     assert requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAI8gKE3AQCSaGhCigcNXRq57OOWmHaT-g").status_code == 200
 
 def test_book_search():
-    result_list = book_search(2)
-    print(result_list)
-    assert result_list == 21
+    '''
+    Test for the book search function.
+
+    The function will take a string and perform a call to the Google Books API, it will then put the resulted books into a list.
+    '''
+    query = "The Way of Kings"
+
+    result_list = book_search(query)
+    
+    assert isinstance(result_list, list)
+    assert len(result_list) > 0
+    assert "volumeInfo" in result_list[0]
