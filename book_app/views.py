58b71e0344256f17487947578045f0797d419dd5
S2486 <skick.bakker@gmail.com>
2025-06-12 10:39:29 +0200

    Http response and requests working

--- book_app/views.py
+++ book_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def get_favorites(request):
     if request.method == "GET":
         print('get request was received')
@@ -15,3 +17,6 @@ def get_favorites(request):
     if request.method == "PUT":
         info = request.body
         return HttpResponse(f'{info} was received PUT')
+    
+    if request.method == "DELETE":
+        return HttpResponse(f'DELETE was used')
